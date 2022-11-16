82ed641de4e29e365b93acd5f61d370c5b888887
Gabriel <hultgabriel@live.se>
2022-11-16 23:28:27 +0100

    day 1 part 2 done finally

--- aoc_1/aoc_1.py
+++ aoc_1/aoc_1.py
@@ -1,10 +1,10 @@
 values = []
 increased = 0
- #test comment for wsl
 
-for line in open('aoc_1_values.txt').readlines():
-    values.append(line)
-    print(line)
+
+for line in open('/home/gult/advent_of_code_2k21/aoc_1/aoc_1_values.txt').readlines():
+    values.append(int(line))
+    #print(line)
 
 #part 1    
 
@@ -20,41 +20,20 @@ print("Value increased", increased, "times!") """
 
 #part 2
 remaining_elements = len(values)
-first_three = []
-second_three = []
-third_three = []
 
 
-for elem in values:
-    if elem == values[0]:
-        first_three.append(elem);
-    else:
-        if len(first_three) % 3 != 0:
-            first_three.append(elem)
+prev_depth_sum = sum(values[0 : 3])
+count_inc = 0
 
-        if len(second_three) % 3 != 0:
-            second_three.append(elem)
+for depth in range(1, len(values) - (3 - 1)):
+    _depths_sum = sum(values[depth : depth + 3])
 
-        if len(third_three) % 3 != 0:
-            third_three.append(elem)
+    if _depths_sum > prev_depth_sum:
+        increased += 1
+    
+    prev_depth_sum = _depths_sum
 
-        print(len(first_three))
-        if len(first_three) % 3 == 0:
-            if first_three[0] + first_three[1] + first_three[2] <  second_three[0] + second_three[1] + second_three[2]:
-                increased += 1
-                print("increased")
-            first_three.clear()
 
-        
-        if len(second_three) % 3 == 0:
-            if second_three[0] + second_three[1] + second_three[2] <  third_three[0] + third_three[1] + third_three[2]:
-                increased += 1
-            second_three.clear()
-
-        if len(third_three) % 3 == 0:
-            if third_three[0] + third_three[1] + third_three[2] <  first_three[0] + first_three[1] + first_three[2]:
-                increased += 1
-            third_three.clear()
 print("Measurements increased", increased, "times!")
 
 
