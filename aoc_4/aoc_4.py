5638594c63004600da0574cbe2996be15d08ad55
Gabriel <hultgabriel@live.se>
2022-11-18 22:29:20 +0100

    day 4 started, not sure how to make this a working nested list yet

---
+++ aoc_4/aoc_4.py
@@ -0,0 +1,17 @@
+
+def main():
+    with open("aoc_4/aoc_4_values.txt", "r") as f:
+        bingoNumbers = f.read().split("\n")
+    print(bingoNumbers[2])
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
