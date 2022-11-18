57fa0a12d44061bc975b168145b3e0ac77929741
Gabriel Hult <hultgabriel@live.se>
2022-11-18 15:14:40 +0100

    3_2 done

--- aoc_3/aoc_3_2.py
+++ aoc_3/aoc_3_2.py
@@ -1,25 +1,66 @@
-values = []
-
 import os
 print(os.getcwd())
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
-lines = os.path.join(dir_path, "aoc_3_values.txt")
-fh = open(lines)
+values = []
+
+
+def main():
+    with open("aoc_3/aoc_3_values.txt", "r") as f:
+        fh = f.read().split("\n")
+    bit = 0
+    a = int(oxygenRating(fh, bit), 2)
+    print("This is our Oxygen generator rating: " + str(a))
+    b = int(scrubberRating(fh, bit), 2)
+    print("This is our C02 scrubber rating: " + str(b))
+
+    print("The life support rating is: " + str(a * b))
+
 
 #Oxygen generator rating
-zero_counter = 0
-one_counter = 0
-
-for binary in fh:
-    if(binary[0] == 1):
-        one_counter += 1
+def oxygenRating(binary_list, bit): 
+    if(len(binary_list) == 1):
+        return binary_list[0]
     else:
-        zero_counter += 1
+        zero_list = []
+        one_list = []
+        zero_counter = 0
+        one_counter = 0 
+        for binary in binary_list:
+            if(binary[bit] == "1"):
+                one_counter += 1
+                one_list.append(binary)
+            else:
+                zero_counter += 1
+                zero_list.append(binary)
+            
+        if(one_counter >= zero_counter):
+            return oxygenRating(one_list, bit+1)
+        else:
+            return oxygenRating(zero_list, bit+1)
 
-if(one_counter > zero_counter):
-    print("To be continued")
+#Oxygen generator rating
+def scrubberRating(binary_list, bit): 
+    if(len(binary_list) == 1):
+        return binary_list[0]
+    else:
+        zero_list = []
+        one_list = []
+        zero_counter = 0
+        one_counter = 0 
+        for binary in binary_list:
+            if(binary[bit] == "1"):
+                one_counter += 1
+                one_list.append(binary)
+            else:
+                zero_counter += 1
+                zero_list.append(binary)
+            
+        if(one_counter < zero_counter):
+            return scrubberRating(one_list, bit+1)
+        else:
+            return scrubberRating(zero_list, bit+1)
 
-
+if __name__ == "__main__":
+    main()
 
     
